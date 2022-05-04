099d9a48a72ace4469d1fe2bb32447b99dd4dcd3
Yoan Lecoq <yoanlecoq.io@gmail.com>
2022-05-04 23:33:38 +0200

    push auto_defrag_allocator

---
+++ c/auto_defrag_allocator.c
@@ -0,0 +1,271 @@
+// gcc -std=c11 -Wall -Wextra -pedantic -Wno-pedantic-ms-format auto_defrag_allocator.c
+
+#include <stdlib.h> // malloc() for main()
+#include <stdint.h>
+#include <stdio.h> // snprintf()
+#include <stdbool.h>
+#include <string.h> // memset(), memcpy()
+#include <inttypes.h>
+#include <assert.h> // static_assert()
+
+#ifdef _WIN32
+#  ifdef _WIN64
+#    define PRI_SIZET PRIu64
+#  else
+#    define PRI_SIZET PRIu32
+#  endif
+#else
+#  define PRI_SIZET "zu"
+#endif
+
+#define dfm_assert assert
+
+//
+// Helper macros
+//
+
+#define FORWARD_DECLARE_STRUCT(T) typedef struct T T
+#define DECLARE_STRUCT(T) FORWARD_DECLARE_STRUCT(T); struct T
+
+//
+// Alignment utilities
+//
+
+// This must work at compile-time (because it is used in static initializers), hence the use of a builtin as a replacement for integer log2().
+// This takes advantage of the fact that alignment values are power-of-two, so they can be stored as a number of bits to shift.
+#define compress_alignment(a) (31 - __builtin_clz((uint32_t) a))
+
+// This operation will never change. Feel free to write (1u << a) directly instead of using this macro.
+#define decompress_alignment(a) (1ul << (a))
+
+// Some tests, in case compress_alignment has to change, due to changing compilers or whatever...
+static_assert((1u << compress_alignment(1)) == 1, "");
+static_assert((1u << compress_alignment(2)) == 2, "");
+static_assert((1u << compress_alignment(4)) == 4, "");
+static_assert((1u << compress_alignment(8)) == 8, "");
+
+static inline uintptr_t align_uintptr(uintptr_t x, uintptr_t a) {
+	return (x + a - 1) & ~(a - 1);
+}
+
+static inline void* align_ptr(const void* x, uintptr_t a) {
+	return (void*) align_uintptr((uintptr_t) x, a);
+}
+
+static inline bool is_ptr_aligned(const void* p, uintptr_t a) {
+	return ((uintptr_t) p) % a == 0;
+}
+
+static inline bool size_is_power_of_two(size_t x) {
+	return x && !(x & (x - 1));
+}
+
+static inline void check_alignment(size_t a) {
+	dfm_assert(a >= 1 && size_is_power_of_two(a));
+}
+
+static inline size_t compress_alignment_checked(size_t a) {
+	check_alignment(a);
+	return compress_alignment(a);
+}
+
+//
+// Defrag allocator
+//
+
+FORWARD_DECLARE_STRUCT(DfmContext);
+
+DECLARE_STRUCT(DfmCallerMetadata) {
+    const char* name;
+};
+
+DECLARE_STRUCT(DfmAllocationParams) {
+    DfmContext* cx;
+    size_t nb_items;
+    size_t sizeof_item;
+    size_t alignof_item;
+    DfmCallerMetadata metadata;
+};
+
+typedef enum DfmEventType {
+    DFM_EVENT__INVALID = 0,
+    DFM_EVENT__ALLOC_FAILED,
+    DFM_EVENT__COUNT // Keep last
+} DfmEventType;
+
+DECLARE_STRUCT(DfmEvent) {
+    DfmEventType event_type;
+    const char* message;
+    const DfmAllocationParams* allocation_params;
+};
+
+typedef void (*DfmEventHandlerFn)(const DfmEvent* e);
+
+DECLARE_STRUCT(DfmSlice) {
+    void* base;
+    size_t size;
+};
+
+DECLARE_STRUCT(DfmSide) {
+    size_t offset_from_base;
+};
+
+DECLARE_STRUCT(DfmBlockHeader) {
+    uint8_t magic;
+};
+
+DECLARE_STRUCT(DfmFreeList) {
+    DfmBlockHeader* first;
+};
+
+DECLARE_STRUCT(DfmContext) {
+    DfmSlice mem;
+    DfmSide left;
+    DfmSide right;
+    DfmFreeList free_list;
+    DfmEventHandlerFn event_handler_fn;
+};
+
+DECLARE_STRUCT(DfmContextParams) {
+    DfmSlice mem;
+    DfmEventHandlerFn event_handler_fn;
+};
+
+void dfm_event_handler_dummy(const DfmEvent* e) {
+    (void) e;
+}
+
+void dfm_init(DfmContext* cx, const DfmContextParams* p) {
+    dfm_assert(cx);
+    dfm_assert(p);
+    dfm_assert(p->event_handler_fn); // If you don't care, please pass dfm_event_handler_dummy. It allows us to avoid testing for NULL every time we want to call it.
+    dfm_assert(cx->mem.size == 0 || cx->mem.base);
+    *cx = (DfmContext) {
+        .mem = p->mem,
+        .event_handler_fn = p->event_handler_fn,
+        .left = { .offset_from_base = 0 },
+        .right = { .offset_from_base = p->mem.size },
+    };
+}
+
+void dfm_deinit(DfmContext* cx) {
+    (void) cx;
+}
+
+static void dfm_cx_notify_alloc_failed(const DfmAllocationParams* p, size_t required_offset_from_base) {
+    char message[512];
+    snprintf(message, sizeof message, "Allocation of size %" PRI_SIZET " x %" PRI_SIZET " would require a total capacity of %" PRI_SIZET ", but we only have %" PRI_SIZET " left\n", p->nb_items, p->sizeof_item, required_offset_from_base, p->cx->right.offset_from_base);
+
+    const DfmEvent event = {
+        .event_type = DFM_EVENT__ALLOC_FAILED,
+        .message = message,
+        .allocation_params = p,
+    };
+    p->cx->event_handler_fn(&event);
+}
+
+void* dfm_alloc_uninitialized_p(const DfmAllocationParams* p) {
+    uint8_t* cursor = p->cx->mem.base;
+    cursor += p->cx->left.offset_from_base;
+    cursor = align_ptr(cursor, p->alignof_item);
+    void* alloc_base = cursor;
+
+    cursor += p->nb_items * p->sizeof_item;
+    const void* alloc_end = cursor;
+
+    const size_t new_offset_from_base = (uint8_t*)alloc_end - (uint8_t*)p->cx->mem.base;
+    if (new_offset_from_base > p->cx->right.offset_from_base) {
+        dfm_cx_notify_alloc_failed(p, new_offset_from_base);
+        return NULL;
+    }
+
+    return alloc_base;
+}
+
+void* dfm_alloc_uninitialized(DfmContext* cx, size_t nb_items, size_t sizeof_item, size_t alignof_item, DfmCallerMetadata metadata) {
+    const DfmAllocationParams p = {
+        .cx = cx,
+        .nb_items = nb_items,
+        .sizeof_item = sizeof_item,
+        .alignof_item = alignof_item,
+        .metadata = metadata,
+    };
+
+    return dfm_alloc_uninitialized_p(&p);
+}
+
+void* dfm_alloc_zeroed_p(const DfmAllocationParams* p) {
+    void* out = dfm_alloc_uninitialized_p(p);
+    if (out)
+        memset(out, 0, p->nb_items * p->sizeof_item);
+
+    return out;
+}
+
+void* dfm_alloc_zeroed(DfmContext* cx, size_t nb_items, size_t sizeof_item, size_t alignof_item, DfmCallerMetadata metadata) {
+    void* out = dfm_alloc_uninitialized(cx, nb_items, sizeof_item, alignof_item, metadata);
+    if (out)
+        memset(out, 0, nb_items * sizeof_item);
+
+    return out;
+}
+
+void dfm_free(DfmContext* cx, void* ptr) {
+    // What we want:
+    // - Ability to visit all blocks sequentially (rare, because it's for profiling/debugging)
+    // - Ability to quickly find the smallest free block that can fit a new allocation; (preferably O(log2 N))
+    // - Free block: ability to get the next (i.e not free) block (for defragmentation)
+    // - Magic number: detect if an address is a valid block
+
+    // Impl:
+    // - Each block stores this struct, with a forced alignment of 1 (compressed/decompressed on-demand):
+    //   - Sentinel for the previous block (1 byte);
+    //     - payload_size : 5; (to jump past it, to access the info struct)
+    //     - magic padding : 3 (it's a magic number and also pads to the next byte)
+    //   - Variable length payload (min 3 bytes, max 24 bytes):
+    //     - nb_items (compressed by choosing the smallest fitting unsigned type)
+    //     - sizeof_item (compressed by choosing the smallest fitting unsigned type)
+    //     - archetype_handle (an index into an array of redirectors)
+    //     - caller metadata??
+    //   - Info for the current block (2 bytes):
+    //     - typeof_nb_items : 2; (0 = u8, 1 = u16, 2 = u32, 3 = u64)
+    //     - typeof_sizeof_item : 2; (0 = u8, 1 = u16, 2 = u32, 3 = u64)
+    //     - typeof_archetype_handle : 2; (0 = u8, 1 = u16, 2 = u32, 3 = u64)
+    //     - is_free : 1;
+    //     - alignof_item : 9; (compressed as a bit shift, since it's power of two).
+    //   - <unused space> (padding for the allocation's alignment, if necessary; some of this bytes are abused for storing an offset, see below)
+    //   - Sentinel just behind the allocation address (1 byte):
+    //     - nb_bits : 3; (0 = none (perfectly aligned), 1 = u8, 2 = u16, 3 = u32, 4 = u64) (indicates a number of bytes to read within the unused space, and cast to unsigned: gives an offset from the allocation pointer, to the address of the info's end)
+    //     - magic : 5;
+    // - This is enough for any block to know where its allocation starts and ends
+}
+
+// TODO: Support for freeing and free space reuse
+// TODO: Support for pointer reachability
+// TODO: Support for defragmentation
+// TODO: Support for multi-threading
+
+//
+// Main
+//
+
+int main() {
+    const size_t mem_capacity = 1024 * 1024;
+    const DfmContextParams cx_params = {
+        .mem = {
+            .base = malloc(mem_capacity),
+            .size = mem_capacity,
+        },
+        .event_handler_fn = dfm_event_handler_dummy,
+    };
+
+    DfmContext cx;
+    dfm_init(&cx, &cx_params);
+
+    char* ptr = dfm_alloc_uninitialized(&cx, 5, 1, 1, (DfmCallerMetadata) {0});
+    memcpy(ptr, "ABCD", 5);
+
+    printf("Allocated %s\n", ptr);
+
+    return 0;
+}
