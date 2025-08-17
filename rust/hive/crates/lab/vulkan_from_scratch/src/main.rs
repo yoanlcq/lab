bbd151bccf770631d1c0d5aa6402b4d61232cc63
Yoan Lecoq <yoanlecoq.io@gmail.com>
2025-08-17 19:01:08 +0200

    vulkan: use mimalloc

--- rust/hive/crates/lab/vulkan_from_scratch/src/main.rs
+++ rust/hive/crates/lab/vulkan_from_scratch/src/main.rs
@@ -1,8 +1,8 @@
 #![expect(unused_crate_dependencies, reason = "This is essentially the same as the library")]
 
 #[global_allocator]
-static GLOBAL_ALLOCATOR: tracing_facade::GlobalAllocatorWrapper<std::alloc::System> =
-    tracing_facade::GlobalAllocatorWrapper::new(std::alloc::System, 100);
+static GLOBAL_ALLOCATOR: tracing_facade::GlobalAllocatorWrapper<mimalloc::MiMalloc> =
+    tracing_facade::GlobalAllocatorWrapper::new(mimalloc::MiMalloc, 100);
 
 fn main() -> vulkan_from_scratch_lib::gpu::Result<()> {
     vulkan_from_scratch_lib::main()
