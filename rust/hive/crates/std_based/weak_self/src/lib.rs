c93cc916875930afc54279b1333b7a28698debd6
Yoan Lecoq <yoanlecoq.io@gmail.com>
2025-08-17 12:45:19 +0200

    weak_self: document

--- rust/hive/crates/std_based/weak_self/src/lib.rs
+++ rust/hive/crates/std_based/weak_self/src/lib.rs
@@ -1,3 +1,31 @@
+//! Convenient wrappers for weak refs for which `upgrade()` can logically never fail.
+//! 
+//! This is only possible when the weak ref is pointing to the object that owns it, hence the name `WeakSelf`.
+//! This is a similar idea to intrusive shared pointers, and mostly a workaround for Rust's lack thereof.
+//! 
+//! The wrappers are quite trivial, their primary purpose is to centralize (and signal use of) that kind of pattern.
+//! The primary benefit is that you are relieved from the responsibility of dealing with `unwrap()/expect()`.
+//! 
+//! # Example
+//! 
+//! ```
+//! # extern crate alloc;
+//! # 
+//! use weak_self::sync::WeakSelf;
+//! use alloc::sync::Arc;
+//! 
+//! struct Me {
+//!     weak_self: WeakSelf<Me>,
+//! }
+//!
+//! let me = Arc::new(Me { weak_self: WeakSelf::new() });
+//! me.weak_self.init(Arc::downgrade(&me));
+//! 
+//! // Upgrading cannot fail, because we followed the intended usage pattern: `weak_self` is pointing to ourselves.
+//! // The only case where this would panic is after `Me` starts being dropped.
+//! let _me = me.weak_self.upgrade_unwrap();
+//! ```
+
 extern crate alloc;
 
 pub mod sync {
@@ -7,6 +35,26 @@ pub mod sync {
     /// A weak reference to an Arc that owns you.
     /// 
     /// It's a pattern where you don't want to create cyclic references, but upgrading the Weak logically cannot ever fail.
+    /// 
+    /// # Example
+    /// 
+    /// ```
+    /// # extern crate alloc;
+    /// # 
+    /// # use weak_self::sync::WeakSelf;
+    /// # use alloc::sync::Arc;
+    /// # 
+    /// struct Me {
+    ///     weak_self: WeakSelf<Me>,
+    /// }
+    ///
+    /// let me = Arc::new(Me { weak_self: WeakSelf::new() });
+    /// me.weak_self.init(Arc::downgrade(&me));
+    /// 
+    /// // Upgrading cannot fail, because we followed the intended usage pattern: `weak_self` is pointing to ourselves.
+    /// // The only case where this would panic is after `Me` starts being dropped.
+    /// let _me = me.weak_self.upgrade_unwrap();
+    /// ```
     #[derive(Debug)]
     pub struct WeakSelf<T>(OnceLock<Weak<T>>);
 
@@ -52,6 +100,26 @@ pub mod rc {
     /// A weak reference to an Arc that owns you.
     /// 
     /// It's a pattern where you don't want to create cyclic references, but upgrading the Weak logically cannot ever fail.
+    /// 
+    /// # Example
+    /// 
+    /// ```
+    /// # extern crate alloc;
+    /// #
+    /// # use weak_self::rc::WeakSelf;
+    /// # use alloc::rc::Rc;
+    /// #
+    /// struct Me {
+    ///     weak_self: WeakSelf<Me>,
+    /// }
+    ///
+    /// let me = Rc::new(Me { weak_self: WeakSelf::new() });
+    /// me.weak_self.init(Rc::downgrade(&me));
+    /// 
+    /// // Upgrading cannot fail, because we followed the intended usage pattern: `weak_self` is pointing to ourselves.
+    /// // The only case where this would panic is after `Me` starts being dropped.
+    /// let _me = me.weak_self.upgrade_unwrap();
+    /// ```
     #[derive(Debug)]
     pub struct WeakSelf<T>(OnceCell<Weak<T>>);
 
